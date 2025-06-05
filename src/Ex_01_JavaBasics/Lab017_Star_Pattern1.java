75a0246c7f315e0f891ef8e612571d6e57055e23
harik <harikasunku21@gmail.com>
2025-06-05 22:40:07 +0530

    Commiting 5st june practice

---
+++ src/Ex_01_JavaBasics/Lab017_Star_Pattern1.java
@@ -0,0 +1,18 @@
+package Ex_01_JavaBasics;
+
+public class Lab017_Star_Pattern1 {
+    public static void main(String[] args) {
+        for(int i=0;i<=2;i++){
+            for(int j=0;j<=2;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+}
+/*
+   ***
+   ***
+   ***
+ */
---
+++ src/Ex_01_JavaBasics/Lab018_Star_Pattern2.java
@@ -0,0 +1,23 @@
+package Ex_01_JavaBasics;
+
+public class Lab018_Star_Pattern2 {
+
+    public static void main(String[] args) {
+    //Lef triangle
+        for(int i=0;i<=2;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+
+
+
+    }
+
+
+
+
+}
